1e046206a54c93ba7ddc3297a7d83d03be2b6755
Seth Berrier <berriers@uwstout.edu>
2019-12-04 14:52:01 -0600

    Update gource details

--- gourceCustomize.sh
+++ gourceCustomize.sh
@@ -3,14 +3,12 @@ cat gourceLog.txt | awk -F\| '$1<=1576108800' > gourceLog.temp
 mv gourceLog.temp gourceLog.txt
 
 # Setup Project Name
-projName="Light in the Darkness - Source Code"
+projName="Light Within the Darkness - Source Code"
 
 function fix {
   sed -i -- "s/$1/$2/g" gourceLog.txt
 }
 
 # Replace non human readable names with proper ones
-# fix "|Berrier|" "|Seth Berrier|"
-# fix "|waterse1080|" "|Eliot Waters|"
-# fix "|CameronPyfferoen|" "|Cameron Pyfferoen|"
-# fix "|Nguyenh0417|" "|Hunter Nguyen|"
+fix "|HalleeAlex|" "|Alex Hallee|"
+fix "|Michael|" "|Michael |"
