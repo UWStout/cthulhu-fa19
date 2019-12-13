f7697af397c4d6d1bb3320aab67b7396756d88b5
Seth Berrier <berriers@uwstout.edu>
2019-12-13 16:23:22 -0600

    Win installer updates

--- buildWinInstaller.sh
+++ buildWinInstaller.sh
@@ -3,16 +3,17 @@
 # Customize for specific game
 dirName=light-within-the-darkness
 pkgName=light-within-the-darkness
+webName=Light_Within_the_Darkness
 
 # Move to dir and rename it
 cd "./packaged/${dirName}/"
 mv win64 "${pkgName}"
 
 # Delete previous archive
-test "../${pkgName}-win64.zip" & rm "../${pkgName}-win64.zip"
+rm -f "../${webName}-win64.zip"
 
 # Create archive
-zip -r9 "../${pkgName}-win64.zip" "./${pkgName}"
+zip -r9 "../${webName}-win64.zip" "./${pkgName}"
 
 # Name back and move back
 mv "${pkgName}" win64
